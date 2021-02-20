7f17f1d10463ae32b6d8bd659714d4e75ff3a057
Mzying2001 <1498932137@qq.com>
2021-02-20 20:57:54 +0800

    解决冲突

--- PvZBackupManager/IniFile.cs
+++ PvZBackupManager/IniFile.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
-using static PvZBackupManager.DllImports;
 
 namespace PvZBackupManager
 {
     class IniFile
     {
+        [DllImport("kernel32")]
+        public static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+
+        [DllImport("kernel32")]
+        public static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
         /// <summary>
         /// ini文件的路径
         /// </summary>
@@ -26,28 +32,15 @@ namespace PvZBackupManager
             Path = path;
         }
 
-        /// <summary>
-        /// 向配置文件写入值
-        /// </summary>
-        public static void ProfileWriteValue(string section, string key, string value, string path)
-        {
-            WritePrivateProfileString(section, key, value, path);
-        }
-
-        /// <summary>
-        /// 读取配置文件的值
-        /// </summary>
-        public static string ProfileReadValue(string section, string key, string path)
-        {
-            StringBuilder sb = new StringBuilder(255);
-            GetPrivateProfileString(section, key, "", sb, 255, path);
-            return sb.ToString().Trim();
-        }
-
         public string this[string section, string key]
         {
-            get => ProfileReadValue(section, key, Path);
-            set => ProfileWriteValue(section, key, value, Path);
+            get
+            {
+                StringBuilder sb = new StringBuilder(255);
+                GetPrivateProfileString(section, key, "", sb, 255, Path);
+                return sb.ToString().Trim();
+            }
+            set => WritePrivateProfileString(section, key, value, Path);
         }
 
         public void WriteValue(string section, string key, object value)
